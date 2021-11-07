ee81ca7e9bd4c86dc09cf5d8abfa8e4fda443d4f
Vianney Accart <vianneyaccart@gmail.com>
2021-11-07 13:45:41 +0100

    resolve exercise from quest 11

--- src/app/cocktail-list/cocktail-list.component.ts
+++ src/app/cocktail-list/cocktail-list.component.ts
@@ -7,21 +7,31 @@ import { Cocktail } from '../shared/models/Cocktail.model';
   templateUrl: './cocktail-list.component.html',
   styleUrls: ['./cocktail-list.component.css'],
 })
-
 export class CocktailListComponent implements OnInit {
+  cocktails: Cocktail[];
 
-  cocktails: Cocktail[] = new Array();
-
-  private service: CocktailService;
-
-  constructor(param_service: CocktailService) {
-    this.service = param_service;
+  constructor(private cocktailService: CocktailService) {
     this.cocktails = new Array();
   }
 
   ngOnInit(): void {
-    this.service.getCocktails().subscribe(
-
-    )
+    // J'utilise la méthode getCocktails() issue de CocktailServices préalablement importé dans le constructor
+    // Je subscribe pour récupérer les données, ici datas (un tableau d'objets issu de cocktails.json)
+    this.cocktailService.getCocktails().subscribe(
+      (datas: any) => {
+        // Pour chaque objet (data) de mon tableau cocktails.json (datas)
+        datas.forEach((data: any) => {
+          // J'ajoute à cocktails (variable locale de type Cocktail)...
+          this.cocktails.push(
+            // ... un nouveau cocktail avec les arguments attendus
+            new Cocktail(data.name, data.price, data.img)
+          );
+        });
+      },
+      // Si le processus ne fonctionne pas, affiche moi l'erreur dans la console
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -2,31 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Cocktail } from './shared/models/Cocktail.model';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CocktailService {
-  cocktails: Cocktail[];
-  private service: HttpClient;
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(param_service: HttpClient) {
-    this.cocktails = new Array();
-    this.service = param_service;
-  }
-
-  getCocktails(): Observable<any> {
-    const obs1: Observable<any> = this.service.get('/assets/cocktails.json');
-    const treatment = (param_data: any) => {
-      // Insérer un new Cocktail ?
-      return (
-        param_data.name as string,
-        param_data.price as number,
-        param_data.img as string
-      );
-    };
-
-    return obs1.pipe(map(treatment));
+  // Récupère les informations du fichier cocktails.json avec le typage Cocktail
+  getCocktails(): Observable<Cocktail[]> {
+    return this.httpClient.get<Cocktail[]>('assets/cocktails.json');
   }
 }
